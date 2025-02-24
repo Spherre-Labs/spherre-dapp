4b02e480ff1d8ca58def330bcdd3b0245fc744be
signor1 <emmanuelomemgboji@gmail.com>
2025-02-24 17:53:34 +0100

    feat:[User Onboarding - Step 1] added step 1

--- frontend/app/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import Nav from "@/components/onboarding/Nav";
-
-export default function UserOnBoarding() {
-    return (
-        <main className="w-full md:px-[50px] px-4 md:pt-[50px] pt-4 overflow-x-hidden">
-            <Nav />
-        </main>
-    )
-}
