020c7483b1ed1c12e26f9075cd2c32ab34bd20ad
signor1 <emmanuelomemgboji@gmail.com>
2025-02-24 14:11:10 +0100

    chore:[User Onboarding Step 1] working on the top nav

---
+++ frontend/app/onboarding/page.tsx
@@ -0,0 +1,7 @@
+export default function UserOnBoarding() {
+    return (
+        <main className="w-full bg-[#101213] md:px-[50px] px-4 md:pt-[61px] pt-4">
+
+        </main>
+    )
+}
---
+++ frontend/components/onboarding/Nav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <header className="w-full flex justify-between items-center"></header>
+    )
+}
+
+export default Nav
