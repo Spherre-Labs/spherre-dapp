9d085abb854e610393e0119b6b2a41c022ac44e8
sebas hidalgo <sebas.hidalgo2004@gmail.com>
2025-03-01 13:32:47 -0600

    feat: create the step 2 page

--- frontend/app/onboarding/step1/page.tsx
+++ frontend/app/onboarding/step1/page.tsx
@@ -1,11 +1,5 @@
-import Nav from '@/components/onboarding/Nav'
 import StepOne from '@/components/onboarding/StepOne'
 
 export default function UserOnBoarding() {
-  return (
-    <main className="w-full lg:px-[50px] md:px-6 px-4 md:py-[50px] py-4 overflow-x-hidden">
-      <Nav />
-      <StepOne />
-    </main>
-  )
+  return <StepOne />
 }
---
+++ frontend/components/onboarding/OnboardingCard.tsx
@@ -0,0 +1,18 @@
+interface OnboardingCardProps {
+  title: string
+  children: React.ReactNode
+}
+
+export default function OnboardingCard({
+  title,
+  children,
+}: OnboardingCardProps) {
+  return (
+    <div className="rounded-[10px] bg-[#1C1D1F] w-full overflow-hidden">
+      <div className="bg-[#272729] py-[18px] md:px-[26px] px-4 w-full h-[62px]">
+        <h4 className="text-white font-[700] text-xl">{title}</h4>
+      </div>
+      {children}
+    </div>
+  )
+}
