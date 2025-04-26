c897913cb683abbba73f80c0a2c0276853235f20
ekene leonard <nwobodoe71@gmail.com>
2025-04-26 15:45:41 +0100

    make stepper reusable

--- frontend/app/components/withdraw-stepper.tsx
+++ frontend/app/components/withdraw-stepper.tsx
@@ -1,59 +1,102 @@
 import React from 'react'
 
-const WithdrawStepper = ({ currentStep }: { currentStep: number }) => {
+interface WithdrawStepperProps {
+  currentStep: number
+  steps: Array<{ step: number; label: string }>
+}
+
+const WithdrawStepper = ({ currentStep, steps }: WithdrawStepperProps) => {
+
   return (
     <div className="flex flex-col items-start md:items-center w-full ">
       <div className="flex flex-col items-start md:flex-row md:items-center md:justify-center  md:space-x-1 mb-4 gap-3 ">
-        <div className="flex items-center md:flex-col gap-3">
+        {steps.map((item, index) => (
           <div
-            className={`w-8 h-8 rounded-full flex items-center justify-center ${
-              currentStep == 1 && 'bg-purple-600'
-            } ${currentStep > 1 && 'bg-green-500'}`}
+            key={new Date().toISOString() + index}
+            className="hidden md:flex items-center"
           >
-            {currentStep > 1 ? '✓' : '1'}
+            <div
+              className={`w-8 h-8 rounded-full flex items-center justify-center bg-gray-600  ${
+                currentStep == item.step && 'bg-purple-600'
+              } ${currentStep > item.step && 'bg-green-500'}`}
+            >
+              {currentStep > item.step ? '✓' : item.step}
+            </div>
+            <span className="ml-2 text-sm text-gray-400 md:hidden">
+              {item.label}
+            </span>
+            {index < steps.length - 1 && (
+              <div
+                className={` hidden md:flex w-28 h-0.5  ${
+                  currentStep > item.step ? 'bg-green-500' : 'bg-gray-600'
+                }`}
+              />
+            )}
           </div>
-          <span className="ml-2 text-sm text-gray-400 md:hidden">
-            Recipient
-          </span>
-        </div>
-        <div
-          className={`hidden md:flex w-28 h-0.5 ${currentStep >= 2 ? 'bg-green-500' : 'bg-gray-600'}`}
-        />
-        <div className="flex md:flex-col items-center gap-3">
-          <div
-            className={`w-8 h-8 rounded-full flex items-center justify-center bg-gray-600 ${
-              currentStep == 2 && 'bg-purple-600'
-            } ${currentStep > 2 && 'bg-green-500'}`}
-          >
-            {currentStep > 2 ? '✓' : '2'}
-          </div>
-          <span className="ml-2 text-sm text-white md:hidden">
-            Token and Amount
-          </span>
-        </div>
-        <div
-          className={`hidden md:flex w-28 md:h-0.5 ${currentStep >= 3 ? 'bg-green-500' : 'bg-gray-600'}`}
-        />
-        <div className="flex items-center md:flex-col gap-3">
-          <div
-            className={`w-8 h-8 rounded-full flex items-center justify-center bg-gray-600 ${
-              currentStep == 3 && 'bg-purple-600'
-            } ${currentStep > 3 && 'bg-green-500'}`}
-          >
-            {currentStep > 3 ? '✓' : '3'}
-          </div>
-          <span className="ml-2 text-sm text-gray-400 md:hidden">
-            Final Review
-          </span>
-        </div>
+        ))}
       </div>
       <div className="hidden md:flex gap-16">
-        <span className="ml-2 text-sm text-gray-400">Recipient</span>
-        <span className="ml-2 text-sm text-white">Token and Amount</span>
-        <span className="ml-2 text-sm text-gray-400">Final Review</span>
+        {steps.map((item, index) => (
+          <span key={new Date().toISOString() + index}
+            className={`ml-2 text-sm text-gray-400 ${currentStep === item.step && 'text-white'}`}
+          >
+            {item.label}
+          </span>
+        ))}
       </div>
     </div>
   )
 }
 
 export default WithdrawStepper
+
+// <div className="flex items-center md:flex-col gap-3">
+// <div
+//   className={`w-8 h-8 rounded-full flex items-center justify-center ${
+//     currentStep == 1 && 'bg-purple-600'
+//   } ${currentStep > 1 && 'bg-green-500'}`}
+// >
+//   {currentStep > 1 ? '✓' : '1'}
+// </div>
+// <span className="ml-2 text-sm text-gray-400 md:hidden">
+//   Recipient
+// </span>
+// </div>
+// <div
+// className={`hidden md:flex w-28 h-0.5 ${currentStep >= 2 ? 'bg-green-500' : 'bg-gray-600'}`}
+// />
+// <div className="flex md:flex-col items-center gap-3">
+// <div
+//   className={`w-8 h-8 rounded-full flex items-center justify-center bg-gray-600 ${
+//     currentStep == 2 && 'bg-purple-600'
+//   } ${currentStep > 2 && 'bg-green-500'}`}
+// >
+//   {currentStep > 2 ? '✓' : '2'}
+// </div>
+// <span className="ml-2 text-sm text-white md:hidden">
+//   Token and Amount
+// </span>
+// </div>
+// <div
+// className={`hidden md:flex w-28 md:h-0.5 ${currentStep >= 3 ? 'bg-green-500' : 'bg-gray-600'}`}
+// />
+// <div className="flex items-center md:flex-col gap-3">
+// <div
+//   className={`w-8 h-8 rounded-full flex items-center justify-center bg-gray-600 ${
+//     currentStep == 3 && 'bg-purple-600'
+//   } ${currentStep > 3 && 'bg-green-500'}`}
+// >
+//   {currentStep > 3 ? '✓' : '3'}
+// </div>
+// <span className="ml-2 text-sm text-gray-400 md:hidden">
+//   Final Review
+// </span>
+// </div>
+// </div>
+// <div className="hidden md:flex gap-16">
+// <span className="ml-2 text-sm text-gray-400">Recipient</span>
+// <span className="ml-2 text-sm text-white">Token and Amount</span>
+// <span className="ml-2 text-sm text-gray-400">Final Review</span>
+// </div>
+// </div>
+// )
--- frontend/app/withdraw/page.tsx
+++ frontend/app/withdraw/page.tsx
@@ -31,11 +31,17 @@ export default function WithdrawPage() {
     router.push('/dashboard')
   }
 
+  const steps = [
+    { step: 1, label: 'Recipient' },
+    { step: 2, label: 'Token and Amount' },
+    { step: 3, label: 'Final Review' },
+  ]
+
   return (
     <div className="min-h-screen bg-[#1C1C1E] text-white pt-16 pl-6">
       <div className="flex items-center mb-6 md:mb-0">
         <Button
-          className="p-2 bg-gray-800 rounded-lg hover:bg-gray-600 transition duration-200 px-5"
+          className="p-2 bg-gray-800 rounded-lg hover:bg-gray-600 transition duration-200 px-[1.25rem]"
           onClick={() => handlePrev()}
         >
           <ArrowLeft className="h-4 w-4 text-white" />
@@ -43,7 +49,7 @@ export default function WithdrawPage() {
         <span className=" font-semibold text-white ml-4">Go Back</span>
       </div>
 
-      <WithdrawStepper currentStep={currentStep} />
+      <WithdrawStepper currentStep={currentStep} steps={steps} />
       {currentStep === 1 && (
         <div className="max-w-2xl mx-auto p-6 space-y-6 flex flex-col items-center">
           <h1 className="text-2xl font-semibold text-white mb-2">
