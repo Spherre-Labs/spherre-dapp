4e9710c2b34ed8826d17ce57b2d36978bc4d9d2c
codebestia <codebestia@gmail.com>
2025-05-29 13:49:45 +0100

    chore: remove unused variables

--- backend/spherre/app/extensions.py
+++ backend/spherre/app/extensions.py
@@ -2,7 +2,6 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema
 
 db = SQLAlchemy()
 migrate = Migrate()
--- backend/spherre/app/utils/decorators.py
+++ backend/spherre/app/utils/decorators.py
@@ -1,6 +1,5 @@
 from functools import wraps
 
-from flask import jsonify
 from flask_jwt_extended import jwt_required
 
 
