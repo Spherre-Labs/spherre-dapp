a83ffa5a9d88f8d010b296e59d1d522819e9a7ff
codebestia <codebestia@gmail.com>
2025-06-28 02:05:59 -0700

    feat: add test blank test functions for testing notifications

---
+++ backend/spherre/tests/test_models/test_notification_model.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from spherre.app import create_app
+from spherre.app.extensions import db
+from spherre.app.models import Notification, NotificationType
+
+
+class TestNotificationModel(TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.ctx.pop()
+
+    def test_create_notification_model():
+        # TODO: write test case for creating notifications
+        pass
+
+    def test_update_notification_model():
+        # TODO: write test case for updating notifications
+        pass
