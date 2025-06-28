f3d42e7df88b6cebcf48ec02d1ce16c1f0319987
codebestia <codebestia@gmail.com>
2025-06-28 01:56:49 -0700

    chore: update model reference in init file

--- backend/spherre/app/models/__init__.py
+++ backend/spherre/app/models/__init__.py
@@ -1,9 +1,23 @@
 from spherre.app.extensions import db
 from spherre.app.models.account import Account, Member
+from spherre.app.models.notification import Notification, NotificationType
+from spherre.app.models.transaction import (
+    Transaction,
+    TransactionStatus,
+    TransactionType,
+)
 
 
 def session_save():
     db.session.commit()
 
 
-__all__ = ["Account", "Member"]
+__all__ = [
+    "Account",
+    "Member",
+    "Notification",
+    "Transaction",
+    "TransactionStatus",
+    "TransactionType",
+    "NotificationType",
+]
