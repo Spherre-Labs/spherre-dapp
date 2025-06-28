3d21407ae7add77971a59c623c5b1aeb6ffcb845
codebestia <codebestia@gmail.com>
2025-06-28 01:06:54 -0700

    feat: add transaction status enum in transaction model

--- backend/spherre/app/models/transaction.py
+++ backend/spherre/app/models/transaction.py
@@ -2,6 +2,7 @@ import enum
 
 from sqlalchemy import Enum
 
+from spherre.app.extensions import db
 from spherre.app.models.base import ModelMixin
 
 
@@ -16,3 +17,14 @@ class TransactionType(enum.Enum):
     MEMBER_ADD = "member_add"
     PERMISSION_EDIT = "permission_edit"
     THRESHOLD_CHANGE = "threshold_change"
+
+
+class TransactionStatus(enum.Enum):
+    """
+    Enum representing the status of a transaction.
+    """
+
+    INITIATED = 1
+    REJECTED = 2
+    APPROVED = 3
+    EXECUTED = 4
