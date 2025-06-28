f7ba18a5928087605889ca6c55ecc5b45ae8fff4
codebestia <codebestia@gmail.com>
2025-06-28 02:04:07 -0700

    feat: add test blank test functions for testing transactions

---
+++ backend/spherre/tests/test_models/test_transaction_model.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from spherre.app import create_app
+from spherre.app.extensions import db
+from spherre.app.models import Transaction, TransactionStatus, TransactionType
+
+
+class TestTransactionModel(TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client()
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.ctx.pop()
+
+    def test_create_transaction_model():
+        # TODO: write test case for creating transactions
+        pass
+
+    def test_update_transaction_model():
+        # TODO: write test case for updating transactions
+        pass
