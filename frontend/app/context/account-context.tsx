9a5436a6a823e214a09c203a97a28138454a3509
codebestia <codebestia@gmail.com>
2025-07-22 10:45:10 -0700

    chore: update account context

--- frontend/app/context/account-context.tsx
+++ frontend/app/context/account-context.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { createContext, useState, ReactNode, useContext } from 'react'
-import { SPHERRE_CONTRACTS } from '@/lib'
+import { createContext, useState, ReactNode, useContext, useEffect } from 'react'
+import { isValidStarknetAddress, SPHERRE_CONTRACTS } from '@/lib'
 import { validateAndParseAddress } from 'starknet'
 import { useParams } from 'next/navigation'
+import { useLocalStorage } from 'usehooks-ts'
 
 export const SpherreAccountContext = createContext<{
   accountAddress: `0x${string}` | null
@@ -20,11 +21,31 @@ export const SpherreAccountProvider = ({
   children,
 }: SpherreAccountProviderProps) => {
   // Declaring the default spherre account address for the main time
-  const [accountAddress, setAccountAddress] = useState<`0x${string}` | null>(
+  const [accountAddress, _setAccountAddress] = useState<`0x${string}` | null>(
     SPHERRE_CONTRACTS.SPHERRE_ACCOUNT,
   )
+
+  const  [value, setValue, removeValue] = useLocalStorage<`0x${string}` | null>('SpherreAccountAddress', null);
   // TODO: Create a functionality to check if the provided accountAddress 
   // is a valid spherre account address.
+  const setAccountAddress = (address: `0x${string}` | null) => {
+    if(address && !isValidStarknetAddress(address)) {
+      console.error('Invalid Starknet address provided:', address);
+      return;
+    }
+    _setAccountAddress(address);
+    setValue(address);
+  }
+  useEffect(() => {
+    if (value){
+      if (isValidStarknetAddress(value)) {
+        _setAccountAddress(value);
+      } else {
+        console.error('Invalid address in local storage:', value);
+        removeValue();
+      }
+    }
+  }, [])
   return (
     <SpherreAccountContext.Provider
       value={{
