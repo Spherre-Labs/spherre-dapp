52daaf1d755b2315841a228c377bb0e678339539
laryhills <hilaryokoh@gmail.com>
2025-07-25 14:30:44 +0100

    chore: enhance transaction component with improved styling, new icons, and responsive design adjustments

--- frontend/app/[address]/transactions/components/transaction.tsx
+++ frontend/app/[address]/transactions/components/transaction.tsx
@@ -19,6 +19,8 @@ import {
 } from '@/lib/contracts/types'
 import { formatTimestamp, formatTime } from '@/lib/utils/transaction-utils'
 import { routes } from '../../layout'
+import { BadgeCheck, ChevronDown, } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 interface TransactionProps {
   transactionInfo: TransactionDisplayInfo
@@ -137,7 +139,7 @@ export default function Transaction({
   }, [isExpanded])
 
   return (
-    <div className="w-full bg-theme-bg-secondary border border-theme-border overflow-hidden transition-colors duration-300 font-sans">
+    <div className="w-full bg-theme-bg-tertiary border border-theme-border overflow-hidden transition-colors duration-300 font-sans">
       <button
         onClick={onToggle}
         className="w-full p-4 lg:p-6 flex items-center gap-[85px] bg-theme-bg-tertiary transition-colors duration-200"
@@ -200,179 +202,122 @@ export default function Transaction({
 
         {/* Dropdown Arrow */}
         <div className="flex-shrink-0 ml-auto">
-          <svg
-            className={`w-4 h-4 sm:w-5 sm:h-5 text-theme-secondary transform transition-transform duration-300 ${isExpanded ? 'rotate-180' : ''
-              }`}
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M19 9l-7 7-7-7"
-            />
-          </svg>
+          <ChevronDown className={`w-4 h-4 sm:w-5 sm:h-5 text-theme-secondary ${isExpanded ? 'rotate-180' : ''}`} />
+
         </div>
 
 
+
       </button>
+      {isExpanded && <div className='bg-[#55534E] my-2 h-[1px] mx-6'></div>}
 
       {/* Expandable content with smooth height transition */}
       <div
         ref={contentRef}
-        className="transition-all duration-300 ease-in-out border-t border-theme-border overflow-hidden"
+        className="transition-all bg-theme-bg-tertiary duration-300 ease-in-out border-t border-theme-border overflow-hidden pb-5"
         style={{ maxHeight: `${contentHeight}px`, opacity: isExpanded ? 1 : 0 }}
       >
-        <div className="flex flex-col lg:flex-row">
+        <div className="flex flex-col mt-3 pb-3 lg:flex-row px-6 font-sans">
           {/* Left: Transaction Progress */}
-          <div className="lg:w-1/2 p-3 sm:p-4 border-theme-border flex flex-col">
-            <h3 className="text-theme font-medium mb-3 sm:mb-4 transition-colors duration-300">
+          <div className="w-[400px] border-theme-border flex flex-col">
+            <h3 className="text-theme font-semibold mb-3 sm:mb-4 transition-colors duration-300">
               Transaction Progress
             </h3>
 
-            {/* Approvals */}
-            <div className="flex flex-col sm:flex-row sm:justify-between gap-2 sm:gap-0">
+            <div className='grid grid-cols-2 gap-2'>
+
+              {/* </div> */}
+              {/* Approvals */}
+              {/* <div className="flex flex-col sm:flex-row sm:justify-between gap-2 sm:gap-0"> */}
               <div className="flex items-center mb-2">
-                <h4 className="text-theme font-medium mr-2  transition-colors duration-300">
+                <h4 className="text-xs text-light-yellow font-semibold mr-2 transition-colors duration-300">
                   Pending Approvals
                 </h4>
-                <div className="border-2 border-theme-border text-theme-secondary text-xs px-2 py-1 sm:px-2.5 sm:py-2 rounded-full ml-2 transition-colors duration-300">
-                  Pending
+                <div className="border-2 border-ash w-5 h-5 text-xs py-[2px] px-[3px] font-bold text-ash flex items-center justify-center rounded-full ml-2 transition-colors duration-300">
+                  +2
                 </div>
               </div>
-              <div className="flex items-center mb-2">
-                <h4 className="text-green font-medium mr-2">
-                  Confirmed Approvals: {transaction.approved.length}
+              <div className="flex justify-between items-center mb-2">
+                <h4 className="text-xs text-green font-semibold mr-2 transition-colors duration-300">
+                  Confirmed Approvals
                 </h4>
-                {transaction.rejected.length > 0 && (
-                  <h4 className="text-red-500 font-medium mr-2  ml-4">
-                    Rejections: {transaction.rejected.length}
-                  </h4>
-                )}
               </div>
-            </div>
 
-            <div className="flex-grow">
-              <div className="flex mb-2 mt-2">
-                <div className="flex">
-                  <div className="flex flex-col items-center mr-3 sm:mr-4">
-                    <div className="w-2 h-2 bg-theme rounded-full flex items-center justify-center mb-1 transition-colors duration-300">
-                      <svg
-                        className="w-4 h-4 sm:w-5 sm:h-5 text-theme"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
+              {/* Txn Timeline */}
+              <div className="flex-grow font-sans">
+                <div className="flex items-start mt-2 space-x-2 mb-0">
+                  <div className="flex flex-col items-center">
+                    <div className="w-[9px] h-[9px] bg-white rounded-full flex items-center justify-center">
                     </div>
-                    <div className="h-full w-0.5 bg-theme-border transition-colors duration-300"></div>
+                    <div className="w-[1px] h-12 bg-white mt-0"></div>
                   </div>
-                  <div>
-                    <p className="text-theme transition-colors duration-300">
+                  <div className="flex-1 pt-0">
+                    <h3 className="text-white flex items-center gap-1 font-semibold -mt-1.5">
                       Initiated Transaction
-                    </p>
-                    <p className="text-theme-secondary  transition-colors duration-300">
+                      <BadgeCheck className="w-5 h-5 text-secondary" fill='#19B360' />
+                    </h3>
+                    <p className="text-theme-secondary font-semibold text-xs transition-colors duration-300">
                       {formatTimestamp(transaction.dateCreated)}{' '}
                       {formatTime(transaction.dateCreated)}
                     </p>
                   </div>
                 </div>
-              </div>
 
-              <div className="flex mb-2 mt-2">
-                <div className="flex">
-                  <div className="flex flex-col items-center mr-3 sm:mr-4">
-                    <div className="w-2 h-3 bg-theme rounded-full flex items-center justify-center mb-1 transition-colors duration-300">
-                      {transaction.status === 'Pending'}
+                <div className="flex items-start mt-0 space-x-2 mb-0">
+                  <div className="flex flex-col items-center">
+                    <div className="w-[9px] h-[9px] bg-white rounded-full flex items-center justify-center">
                     </div>
-                    <div className="h-full w-0.5 bg-theme-border transition-colors duration-300"></div>
+                    <div className={`w-[1px] h-12 mt-0 ${transaction.status === 'Pending' ? 'bg-[#55534E]' : 'bg-white'}`}></div>
                   </div>
-                  <div>
-                    <p className="text-theme transition-colors duration-300">
-                      {transaction.status}
+                  <div className="flex-1 pt-0">
+                    <h3 className="text-white font-semibold -mt-1.5">Pending</h3>
+                    <p className="text-theme-secondary font-semibold text-xs transition-colors duration-300">
+                      {`Threshold: ${transaction.approved.length} / ${Math.min(5, transaction.approved.length * 5)} approved`}
                     </p>
-                    <div className="flex items-center text-theme-secondary  transition-colors duration-300">
-                      <p>
-                        Approvals: {transaction.approved.length} | Rejections:{' '}
-                        {transaction.rejected.length}
-                      </p>
+                  </div>
+                </div>
+
+                <div className="flex items-start mt-0 space-x-2 mb-0">
+                  <div className="flex flex-col items-center">
+                    <div className={`w-[9px] h-[9px] rounded-full flex items-center justify-center
+                    ${transaction.status !== 'Executed' ? 'border border-[#55534E]' : 'bg-white'}
+                    `}>
                     </div>
+                  </div>
+                  <div className="flex-1 pt-0">
+                    <h3 className="text-white font-semibold -mt-1.5">Execution</h3>
+                    <p className="text-theme-secondary font-semibold text-xs transition-colors duration-300">
+                      ---------------------
+                    </p>
                   </div>
                 </div>
               </div>
 
-              <div className="flex mb-2 mt-2">
-                <div className="flex">
-                  <div className="flex flex-col items-center mr-3 sm:mr-4">
-                    <div
-                      className={`w-2 h-2 border border-theme-border ${transaction.status === 'Executed' ? 'bg-theme' : ''
-                        } rounded-full flex items-center justify-center mb-1 transition-colors duration-300`}
-                    >
-                      {transaction.status === 'Executed' ? (
-                        <svg
-                          className="w-4 h-4 sm:w-5 sm:h-5 text-theme"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                          />
-                        </svg>
-                      ) : null}
-                    </div>
-                  </div>
-                  <div>
-                    <p
-                      className={`transition-colors duration-300 ${transaction.status === 'Executed'
-                        ? 'text-theme'
-                        : 'text-theme-secondary'
-                        }`}
-                    >
-                      Executed
-                    </p>
-                    {transaction.dateExecuted && (
-                      <p className="text-theme-secondary  transition-colors duration-300">
-                        {formatTimestamp(transaction.dateExecuted)}{' '}
-                        {formatTime(transaction.dateExecuted)}
-                      </p>
-                    )}
-                  </div>
-                </div>
-              </div>
+
+              <h4 className="text-xs text-theme-secondary font-semibold mr-2">
+                Rejections: ({transaction.rejected.length})
+              </h4>
             </div>
+
 
             {/* Action Buttons */}
             <div className="mt-auto pt-3 sm:pt-4">
               {transaction.status === 'Pending' && (
                 <div className="flex flex-col sm:flex-row gap-2 sm:gap-3">
-                  <button
+                  <Button
                     onClick={handleApprove}
                     disabled={isApproving}
-                    className="bg-[#6F2FCE] hover:bg-purple-700 text-white px-4 sm:px-6 py-2 rounded-md transition duration-200 w-full sm:w-1/3 disabled:opacity-50"
+                    className=" text-white px-4 sm:px-6 py-2.5 rounded-md transition duration-200 w-full"
                   >
                     {isApproving ? 'Approving...' : 'Approve'}
-                  </button>
-                  <button
+                  </Button>
+                  <Button
                     onClick={handleReject}
                     disabled={isRejecting}
-                    className="bg-theme-bg-tertiary hover:bg-theme-border text-theme px-4 sm:px-6 py-2 rounded-md transition duration-200 w-full sm:w-1/3 border border-theme-border disabled:opacity-50"
+                    className="bg-theme-bg-secondary hover:bg-theme-bg-secondary text-theme px-4 sm:px-6 py-2.5 rounded-md transition duration-200 w-full disabled:opacity-50"
                   >
                     {isRejecting ? 'Rejecting...' : 'Reject'}
-                  </button>
-                  <button
-                    onClick={handleExecute}
-                    disabled={isExecuting}
-                    className="bg-green-600 hover:bg-green-700 text-white px-4 sm:px-6 py-2 rounded-md transition duration-200 w-full sm:w-1/3 disabled:opacity-50"
-                  >
-                    {isExecuting ? 'Executing...' : 'Execute'}
-                  </button>
+                  </Button>
                 </div>
               )}
               {(transaction.status === 'Executed' ||
@@ -384,8 +329,10 @@ export default function Transaction({
             </div>
           </div>
 
+          <div className='bg-[#55534E] w-0.5 my-2 mx-6'></div>
+
           {/* Right: Transaction Details */}
-          <div className="lg:w-1/2 p-3 sm:p-4 flex flex-col">
+          <div className="lg:w-2/3 flex flex-col">
             <h3 className="text-theme font-medium mb-3 sm:mb-4 transition-colors duration-300">
               Transaction Details
             </h3>
@@ -429,9 +376,10 @@ export default function Transaction({
                 transaction.id.toString(),
               )}
             >
-              <button className="mt-3 sm:mt-4 w-full bg-theme-bg-tertiary hover:bg-theme-border text-theme py-2 rounded-lg transition-colors border border-theme-border">
+              <Button className="mt-3 hover:bg-theme-bg-secondary bg-theme-bg-secondary text-theme px-4 sm:px-6 py-2.5 rounded-md transition duration-200 w-full disabled:opacity-50"
+              >
                 See transaction details
-              </button>
+              </Button>
             </Link>
           </div>
         </div>
