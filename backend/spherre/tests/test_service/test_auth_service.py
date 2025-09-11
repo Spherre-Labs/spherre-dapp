30b38a017710a7c7b26304a21ee1f22e547809d5
codebestia <codebestia@gmail.com>
2025-09-11 15:44:44 -0700

    chore: format codebase

--- backend/spherre/tests/test_service/test_auth_service.py
+++ backend/spherre/tests/test_service/test_auth_service.py
@@ -1,9 +1,11 @@
 from unittest import TestCase
+
 from flask_jwt_extended import create_access_token, create_refresh_token
 
 from spherre.app import create_app
 from spherre.app.extensions import db
 from spherre.app.service.auth import AuthService
+
 
 class TestAuthService(TestCase):
     def setUp(self):
@@ -22,8 +24,11 @@ class TestAuthService(TestCase):
         data = AuthService.sign_in_member("0x123")
         assert data is not None
         assert data["member"] == "0x123"
-        
+
     def test_generate_address_from_public_key(self):
         address = AuthService.generate_address_from_public_key("0x123")
         assert address is not None
-        assert address == "0x4809f483eca7515a989e9ad1509708563bbdd972af975ffcc8fbd193aa47710"
+        assert (
+            address
+            == "0x4809f483eca7515a989e9ad1509708563bbdd972af975ffcc8fbd193aa47710"
+        )
