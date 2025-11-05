dc6d8b37971bef4e84cb44f80d9f12dc8e66dc48
codebestia <codebestia@gmail.com>
2025-11-05 08:03:30 +0100

    feat: create notification preference serializer

--- backend/spherre/app/serializers/notifications.py
+++ backend/spherre/app/serializers/notifications.py
@@ -5,6 +5,10 @@ class AccountSchema(Schema):
     address = fields.String()
 
 
+class MemberSchema(Schema):
+    address = fields.String()
+
+
 class NotificationSchema(Schema):
     id = fields.String()
     notification_type = fields.String()
@@ -13,3 +17,10 @@ class NotificationSchema(Schema):
     account = fields.Nested(AccountSchema)
     created_at = fields.DateTime()
     read_by = fields.List(fields.String())
+
+
+class NotificationPreferenceSchema(Schema):
+    id = fields.String()
+    account = fields.Nested(AccountSchema)
+    member = fields.Nested(MemberSchema)
+    email_enabled = fields.Boolean()
